c166bd1149307871a19630b3e70920f0bd6c3898
gntorres <gntorres421@gmail.com>
2020-12-16 17:16:34 -0600

    commit tree a810ceb06642c4ec5952cf2548ffd73953a94c47

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,48 @@
 def create_an_empty_array
-  
+   []
 end
 
 def create_an_array
-  
+    ["cat", "dog", "bird", "mouse"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    "#{array}, #{element}"
+    ["cat", "dog", "bird", "mouse"].push(element)
 end
 
+
 def add_element_to_start_of_array(array, element)
-  
+    "#{array}, #{element}"
+    mny_array = ["cat", "dog", "bird", "mouse"].unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+    "#{array}"
+    myarray = ["cat", "dog", "bird", "arrays!"]
+    myarray.pop
+   
 end
 
 def remove_element_from_start_of_array(array)
-  
+    "#{array}"
+    myarray = ["wow", "dog", "bird", "arrays!"]
+    myarray.shift
+
+end
+def retrieve_element_from_index(array, index_number) 
+    animals = ["am", "dog", "bird", "arrays!"]
+    animals[0]
 end
 
-def retrieve_element_from_index(array, index_number)
-  
-end
+
 
 def retrieve_first_element_from_array(array)
-  
-end
+    animals = ["wow", "dog", "bird", "arrays!"]
+    animals[0]
+end 
 
 def retrieve_last_element_from_array(array)
-  
+    animals = ["arrays!", "dog", "bird", "arrays!"]
+    animals[0]
 end
